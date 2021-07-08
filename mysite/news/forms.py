ed5e6033ce28e247a5d651c7fe79dc96b332c266
AnViSe <anvise@grodno.belpost.by>
2021-07-08 17:11:08 +0300

    Урок 26-27

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 # from .models import Category
 from .models import News
+import re
+from django.core.exceptions import ValidationError
 
 
 class NewsForm(forms.ModelForm):
@@ -27,3 +29,10 @@ class NewsForm(forms.ModelForm):
     #                                   label='Категория',
     #                                   empty_label='Сделайте выбор',
     #                                   widget=forms.Select({"class": "form-control"}))
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
+
