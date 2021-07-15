c97fa013996e38cda67a998b6663cc664c3ed621
AnViSe <anvise@grodno.belpost.by>
2021-07-15 11:17:42 +0300

    Урок 46

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,8 +1,33 @@
 from django import forms
-# from .models import Category
-from .models import News
-import re
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+import re
+
+from .models import News
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя',
+                               help_text='Подсказка',
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Пароль',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='E-mail',
+                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        # widgets = {
+        #     'username': forms.TextInput(attrs={"class": "form-control"}),
+        #     'email': forms.EmailInput(attrs={"class": "form-control"}),
+        #     'password1': forms.PasswordInput(attrs={"class": "form-control"}),
+        #     'password2': forms.PasswordInput(attrs={"class": "form-control"}),
+        # }
 
 
 class NewsForm(forms.ModelForm):
@@ -11,9 +36,9 @@ class NewsForm(forms.ModelForm):
         # fields = '__all__'
         fields = ['title', 'content', 'is_published', 'category']
         widgets = {
-            'title': forms.TextInput(attrs={"class": "form-control"}),
-            'content': forms.Textarea(attrs={"class": "form-control", "rows": 5}),
-            'category': forms.Select({"class": "form-control"}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', "rows": 5}),
+            'category': forms.Select({'class': 'form-control'}),
         }
 
     # title = forms.CharField(max_length=150,
